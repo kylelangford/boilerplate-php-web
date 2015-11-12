3d82bb99c6cc338b91f9738340e058a047d6b079
KyleLangford <kyle.langford@brunellocreative.com>
2015-11-11 23:50:20 -0500

    added mailer functionality, not tested

--- index.php
+++ index.php
@@ -24,10 +24,49 @@ $view->parserExtensions = array(
 
 $app->get('/', function() use($app) {
   $app->render('about.twig');
-});
+})->name('home');
+
 
 $app->get('/contact', function() use($app)  {
   $app->render('contact.twig');
+})->name('contact');
+
+$app->post('/contact', function() use($app)  {
+  $name = $app->request->post('name');
+  $email = $app->request->post('email');
+  $msg = $app->request->post('msg');
+
+  if(!empty($name) && !empty($email)  && !empty($msg)) {
+    $cleanName = filter_var($name, FILTER_SANITIZE_STRING);
+    $cleanEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $cleanMsg = filter_var($msg, FILTER_SANITIZE_STRING);
+
+  } else {
+    // show error
+    $app->redirect('/contact');
+  }
+
+  $transport = Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
+  $mailer = \Swift_Mailer::newInstance($transport);
+
+  $message = \Swift_Message::newInstance();
+  $message->setSubject('Email from our website');
+  $message->setForm(array(
+    $cleanEmail => $cleanName
+  ));
+  $message->setTo(array('treehouse@localhost'));
+  $message->setBody($cleanMsg);
+
+  $result = $mailer->send($message);
+
+  if($result > 0) {
+    // Send msg to user
+    $app->redirect('/');
+  } else {
+    // Send msg to user
+    // Log error
+    $app->redirect('/contact');
+  }
 });
 
 $app->run();
